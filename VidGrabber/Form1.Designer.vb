ac5fa11ee6cfdf27a12b143d3da120f4d8c42999
kkobz <kkobz@DESKTOP-8HHVL5C>
2018-05-03 07:18:44 -0700

    upload

--- VidGrabber/Form1.Designer.vb
+++ VidGrabber/Form1.Designer.vb
@@ -22,10 +22,120 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.urlbox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.gobutt = New System.Windows.Forms.Button()
+        Me.vidurl = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.vidname = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.downloadbutt = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'urlbox
+        '
+        resources.ApplyResources(Me.urlbox, "urlbox")
+        Me.urlbox.Name = "urlbox"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'gobutt
+        '
+        resources.ApplyResources(Me.gobutt, "gobutt")
+        Me.gobutt.Name = "gobutt"
+        Me.gobutt.UseVisualStyleBackColor = True
+        '
+        'vidurl
+        '
+        resources.ApplyResources(Me.vidurl, "vidurl")
+        Me.vidurl.Name = "vidurl"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'vidname
+        '
+        resources.ApplyResources(Me.vidname, "vidname")
+        Me.vidname.Name = "vidname"
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'downloadbutt
+        '
+        resources.ApplyResources(Me.downloadbutt, "downloadbutt")
+        Me.downloadbutt.Name = "downloadbutt"
+        Me.downloadbutt.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'PictureBox1
+        '
+        resources.ApplyResources(Me.PictureBox1, "PictureBox1")
+        Me.PictureBox1.Image = Global.VidGrabber.My.Resources.Resources.vidgrabber
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.downloadbutt)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.vidname)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.vidurl)
+        Me.Controls.Add(Me.gobutt)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.urlbox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents urlbox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents gobutt As Button
+    Friend WithEvents vidurl As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents vidname As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents downloadbutt As Button
+    Friend WithEvents Label4 As Label
 End Class
--- VidGrabber/Form1.vb
+++ VidGrabber/Form1.vb
@@ -1,3 +1,68 @@
-﻿Public Class Form1
+﻿Imports System.Net
+Imports System.Text.RegularExpressions
 
+Public Class Form1
+    Dim webclient1 As New WebClient
+    Dim urltemp As String
+    Dim htmltemp As String
+    Dim vidtitleval As String
+
+    Public Property Vidtitleval1 As String
+        Get
+            Return vidtitleval
+        End Get
+        Set(value As String)
+            vidtitleval = value
+        End Set
+    End Property
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        downloadbutt.Enabled = False
+        vidname.Text = ""
+        vidurl.Text = ""
+    End Sub
+
+    Private Sub gobutt_Click(sender As Object, e As EventArgs) Handles gobutt.Click
+        urltemp = urlbox.Text
+        Try
+            htmltemp = webclient1.DownloadString(urltemp)
+        Catch ex As Exception
+            MessageBox.Show("No connection to the video provider!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            vidname.Text = ""
+            vidurl.Text = ""
+            htmltemp = ""
+            vidtitleval = ""
+            urltemp = ""
+            urlbox.Text = ""
+            gobutt.Enabled = True
+            downloadbutt.Enabled = True
+            Exit Sub
+        End Try
+        vidtitleval = Regex.Match(htmltemp, "\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups("Title").Value
+        vidname.Text = vidtitleval
+        vidurl.Text = urltemp
+        gobutt.Enabled = False
+        downloadbutt.Enabled = True
+    End Sub
+
+    Private Sub downloadbutt_Click(sender As Object, e As EventArgs) Handles downloadbutt.Click
+        downloadbutt.Enabled = False
+        Dim processstring As String
+        processstring = "-f mp4 -o ""%USERPROFILE%\Videos\%(title)s.%(ext)s"" " + urltemp
+        Try
+            Process.Start("youtube-dl.exe", processstring)
+        Catch ex As Exception
+            MessageBox.Show("The file youtube-dl.exe could not be found or cannot be run!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            vidname.Text = ""
+            vidurl.Text = ""
+            htmltemp = ""
+            vidtitleval = ""
+            urltemp = ""
+            urlbox.Text = ""
+            gobutt.Enabled = True
+            downloadbutt.Enabled = True
+            Exit Sub
+        End Try
+        MessageBox.Show("Video was saved to Videos folder.", "Video Saved", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
